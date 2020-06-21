ee0f24e5f29833b2adbe2192cca55eac17eeba79
Caroline Barcelos <barceloscarol.cbg@gmail.com>
2020-06-21 11:18:12 -0300

    Alteracao scale by ratio

--- prepareDataDictionary.py
+++ prepareDataDictionary.py
@@ -19,7 +19,7 @@ def mainPrepareDictionaryData():
     
     filteredSaudaveisDicData, filteredDoentesDicData, deltaT, min10mean = preprocessDictionaryDataset(saudaveisDictionaryData, doentesDictionaryData)
     trainData, trainTarget, testData, testTarget, validationData, validationTarget = splitData(shuffleSeed, filteredSaudaveisDicData, filteredDoentesDicData)
-    trainData, testData, validationData = minMaxNormalization(trainData, testData, validationData, deltaT, min10mean)
+    #trainData, testData, validationData = minMaxNormalization(trainData, testData, validationData, deltaT, min10mean)
     
     trainLoader, testLoader, validationLoader, n_classes, cat_df = prepareNumpyDatasetBalancedData(trainData, trainTarget, testData, testTarget, validationData, validationTarget, batch_size)
     return trainLoader, testLoader, validationLoader, n_classes, cat_df, batch_size, max_epochs_stop, n_epochs
@@ -27,8 +27,8 @@ def mainPrepareDictionaryData():
 def mainReadData():
     print('\nprepareDataFromTXT')
     txt_saudaveis_files, txt_doentes_files= getFilesName()
-    saudaveisDictionaryData = readFilesByPatient(txt_saudaveis_files)
-    doentesDictionaryData = readFilesByPatient(txt_doentes_files)
+    saudaveisDictionaryData = readFilesByPatient(txt_saudaveis_files, 'saudaveis')
+    doentesDictionaryData = readFilesByPatient(txt_doentes_files, 'doentes')
     return saudaveisDictionaryData, doentesDictionaryData
 
 def getFilesName():
@@ -40,7 +40,7 @@ def getFilesName():
     
     return txt_saudaveis_files, txt_doentes_files
 
-def readFilesByPatient(txt_files):
+def readFilesByPatient(txt_files, patientClass):
     print('readFiles')
     dataAsDictionary = {}
     for i in range(len(txt_files)):
@@ -49,7 +49,7 @@ def readFilesByPatient(txt_files):
         patientId = fileName.split('.')[0]
         inputData = np.loadtxt(txt_files[i], dtype='f', delimiter=' ')
         #print('antes inputData', inputData.shape)
-        #inputData = convertTeste(inputData, i)
+        inputData = preProcessingWithRatio(inputData, i, patientClass)
         #Stack the data to simulate 3d image
         inputData = np.stack((inputData,)*3, axis=2)
         inputData = np.transpose(inputData, (2, 0, 1))
@@ -66,11 +66,16 @@ def readFilesByPatient(txt_files):
             dataAsDictionary[patientId].append(inputData)
     return dataAsDictionary
 
-def convertTeste(image, i):
+def preProcessingWithRatio(image, i, type):
     numpyImage = image
-    max=np.amax(numpyImage)
+
+    flattenDataset = numpyImage.flatten()
+    indices = np.argpartition(flattenDataset, -100)[-100:] 
+    topValues = flattenDataset[indices]
+    topValues = np.mean(topValues)
+    #print('topValues', topValues, ' size ', topValues.size)
     # # scale by ratio of 255/max to increase to fully dynamic range
-    numpyImage = ((255/max)*numpyImage).clip(0,255)
+    numpyImage = ((255/topValues)*numpyImage).clip(0,255)
     #numpyImage2 = ((255/max)*numpyImage)
     #print('numpyImage min = ', numpyImage.min(), 'max = ', numpyImage.max())
     #print('numpyImage2 min = ', numpyImage2.min(), 'max = ', numpyImage2.max())
@@ -79,9 +84,8 @@ def convertTeste(image, i):
     # imOut = cv2.cvtColor(numpyImage, cv2.COLOR_GRAY2RGB)
     # print('imOut', imOut)
     numpyImage = numpyImage.astype(np.uint8)
-    plotTestTransformedImages(numpyImage, 'testecv2'+ str(i))
+    #plotTestTransformedImages(numpyImage, type + '_preProcessedImage'+ str(i))
     return numpyImage
-
 
 def splitData(shuffleSeed, saudaveisData, doentesData):
     print('\nSplit Healthy Dataset')
@@ -189,12 +193,12 @@ def splitPatientsFromDictionary(shuffleSeed, dictionaryData):
     return indicesTreinamento, indicesTeste, indicesValidacao
 
 def minMaxNormalization(dataTrain, dataTest, dataValidation, deltaT, min10mean):
-    #dataTrain = (dataTrain)/deltaT
-    dataTrain = (dataTrain-min10mean)/deltaT
-    #dataTest = (dataTest)/deltaT
-    dataTest = (dataTest-min10mean)/deltaT
-    #dataValidation = (dataValidation)/deltaT
-    dataValidation = (dataValidation-min10mean)/deltaT
+    dataTrain = (dataTrain)/deltaT
+    #dataTrain = (dataTrain-min10mean)/deltaT
+    dataTest = (dataTest)/deltaT
+    #dataTest = (dataTest-min10mean)/deltaT
+    dataValidation = (dataValidation)/deltaT
+    #dataValidation = (dataValidation-min10mean)/deltaT
     return dataTrain, dataTest, dataValidation
 
 def prepareNumpyDatasetBalancedData(dataTrain, dataTargetTrain, dataTest, dataTargetTest, dataValidation, dataTargetValidation, batch_size):
@@ -217,18 +221,18 @@ def prepareNumpyDatasetBalancedData(dataTrain, dataTargetTrain, dataTest, dataTa
     #     #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # Imagenet standards
     # ])
     
-    # defaultTransform = transforms.Compose([
-    #     transforms.ToPILImage(),
-    #     transforms.ToTensor(),  # Imagenet standards  # Imagenet standards
-    # ])
+    defaultTransform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.ToTensor(),  # Imagenet standards 
+    ])
 
-    trainDataset = CustomDatasetFromNumpyArray(dataTrain,dataTargetTrain)
+    trainDataset = CustomDatasetFromNumpyArray(dataTrain, dataTargetTrain, defaultTransform)
     trainLoader = DataLoader(trainDataset, batch_size=batch_size, shuffle=True)
 
-    testDataset = CustomDatasetFromNumpyArray(dataTest,dataTargetTest)
+    testDataset = CustomDatasetFromNumpyArray(dataTest, dataTargetTest, defaultTransform)
     testLoader = DataLoader(testDataset, batch_size=batch_size, shuffle=True)
 
-    validationDataset = CustomDatasetFromNumpyArray(dataValidation, dataTargetValidation)
+    validationDataset = CustomDatasetFromNumpyArray(dataValidation, dataTargetValidation, defaultTransform)
     validationLoader = DataLoader(validationDataset, batch_size=batch_size, shuffle=True)
 
     resultLabelsTraining = torch.zeros(2, dtype=torch.long)
@@ -238,7 +242,7 @@ def prepareNumpyDatasetBalancedData(dataTrain, dataTargetTrain, dataTest, dataTa
         l = labels.numpy()
         resultLabelsTraining[0] = resultLabelsTraining[0] + np.count_nonzero(l == 0)
         resultLabelsTraining[1] = resultLabelsTraining[1] + np.count_nonzero(l == 1)
-        #plotTransformedImages(images, i, 'experimentos_train')
+        #plotTransformedImages(images, i, 'transformed_preProcessingWithRatio_train')
         i = i+1
 
     i=0
@@ -247,7 +251,7 @@ def prepareNumpyDatasetBalancedData(dataTrain, dataTargetTrain, dataTest, dataTa
         l = labels.numpy()
         resultLabelsTesting[0] = resultLabelsTesting[0] + np.count_nonzero(l == 0)
         resultLabelsTesting[1] = resultLabelsTesting[1] + np.count_nonzero(l == 1)
-        #plotTransformedImages(images, i, 'experimentos_test')
+        #plotTransformedImages(images, i, 'transformed_preProcessingWithRatio_test')
         i = i+1
 
     resultLabelsValidation = torch.zeros(2, dtype=torch.long)
@@ -256,7 +260,7 @@ def prepareNumpyDatasetBalancedData(dataTrain, dataTargetTrain, dataTest, dataTa
         l = labels.numpy()
         resultLabelsValidation[0] = resultLabelsValidation[0] + np.count_nonzero(l == 0)
         resultLabelsValidation[1] = resultLabelsValidation[1] + np.count_nonzero(l == 1)
-        #plotTransformedImages(images, i, 'experimentos_validation')
+        #plotTransformedImages(images, i, 'transformed_preProcessingWithRatio_validation')
         i = i+1
 
     cat_df = pd.DataFrame({
