9a7b51879f43c905c94899c8c9141b7001b0acb4
Caroline Barcelos <barceloscarol.cbg@gmail.com>
2020-09-26 18:58:30 -0300

    Ajustes matlab code to augment data

--- Matlab Code/colormap_jet_convert_python.m
+++ Matlab Code/colormap_jet_convert_python.m
@@ -3,7 +3,7 @@
 %% a
 A = load('../../Imagens_TXT_Estaticas_Balanceadas/0Saudavel/T0275.1.1.S.2015-03-13.00.txt') ; 
 I = A;
-figure;
+
 %% 
 f = figure;
 cmap = colormap(f,jet);
@@ -14,7 +14,7 @@ colorbar
 % 
 % F = getimage;
 % dadosFrame = F.cdata;
-
+    
 Cdata = h.CData;
 cmap = colormap;
 
@@ -42,7 +42,21 @@ histogram(RGB);
 %No python: x = np.load('numpyTesting.npy')
 
 %% utilizando imageDataAugmenter
-augmenter = imageDataAugmenter('RandRotation',[0 360], ...
+I = A;
+RGBParsed = RGB;
+thresh = multithresh(I);
+seg_I = imquantize(I,thresh);
+figure;imagesc(seg_I);
+seg_I(seg_I == 2) = 0;
+figure;imagesc(seg_I);
+imgComMask = RGBParsed.* seg_I;
+figure;imagesc(imgComMask)
+meanValue1 = mean(nonzeros(imgComMask(:,:,1)));
+meanValue2 = mean(nonzeros(imgComMask(:,:,2)));
+meanValue3 = mean(nonzeros(imgComMask(:,:,3)));
+
+augmenter = imageDataAugmenter('FillValue', [meanValue1, meanValue2, meanValue3],...
+    'RandRotation',[0 360], ...
     'RandXReflection',true, ...
     'RandYReflection', true, ...
     'RandRotation',[-45, 45] ...
@@ -51,6 +65,88 @@ outCellArray = augment(augmenter,{RGB});
 outImg = imtile(outCellArray);
 figure; imshow(outImg);
 
-%%
+%% 
 imJittered = jitterColorHSV(RGB,'Saturation',[-0.4 -0.1]); 
 montage({RGB,imJittered})
+
+
+%% 
+% top = mean(maxk(I(:),100));
+% I = uint8((255/top)*I);
+% imagesc(I)
+
+% [L,Centers] = imsegkmeans(I,2, 'NormalizeInput', true);
+% B = labeloverlay(I,L);
+% fig = figure;
+% imshow(B)
+% 
+% level=graythresh(I);
+% BW = imbinarize(I,level);
+
+I = A;
+%I = medfilt2(I);
+
+%Nro de thresholds
+%%
+I = A;
+top = mean(maxk(I(:),100));
+I = uint8((255/top)*I);
+imagesc(I)
+% top = mean(maxk(RGB(:),100));
+% I= uint8((255/top)*RGB);
+% imagesc(I)
+
+[L,Centers] = imsegkmeans(I,2, 'NormalizeInput', true);
+B = labeloverlay(I,L);
+fig = figure;
+imshow(B)
+I1 = B;
+values = I1(L == 1);
+meanValue1 = uint8(mean(values));
+% values = I2(L == 1);
+% meanValue2 = uint8(mean(values));
+% values = I3(L == 1);
+% meanValue3 = uint8(mean(values));
+
+% values = I(L == 1);
+% meanValue = uint8(mean(values));
+% meanValue1 = mean(nonzeros(imgComMask(:,:,1)));
+% meanValue2 = mean(nonzeros(imgComMask(:,:,2)));
+% meanValue3 = mean(nonzeros(imgComMask(:,:,3)));
+
+%%
+
+I = A;
+RGBParsed = RGB;
+thresh = multithresh(I);
+seg_I = imquantize(I,thresh);
+figure;imagesc(seg_I);
+RGBParsed1 = RGBParsed(:,:,1);
+RGBParsed2 = RGBParsed(:,:,2);
+RGBParsed3 = RGBParsed(:,:,3);
+meanValue1 = mean(RGBParsed1(seg_I == 1));
+meanValue2 = mean(RGBParsed2(seg_I == 1));
+meanValue3 = mean(RGBParsed3(seg_I == 1));
+
+% teste = RGBParsed(seg_I == 2);
+% figure;imagesc(seg_I);
+% imgComMask = RGBParsed.* seg_I;
+% figure;imagesc(imgComMask)
+% meanValue1 = mean(nonzeros(imgComMask(:,:,1)));
+% meanValue2 = mean(nonzeros(imgComMask(:,:,2)));
+% meanValue3 = mean(nonzeros(imgComMask(:,:,3)));
+%%
+
+imOriginal = RGB;
+tform = randomAffine2d('Rotation',[-45 45], 'XReflection',true,'YReflection',true); 
+outputView = affineOutputView(size(imOriginal),tform);
+imAlterada = imwarp(imOriginal,tform,'OutputView',outputView,'FillValues',[meanValue1 meanValue2 meanValue3]);
+figure
+imagesc(imAlterada)
+title('sem cores')
+
+imAlteradaCor = jitterColorHSV(imAlterada,'Contrast',[1.2 1.4],'Saturation',[-0.4 -0.1],'Brightness',[-0.2 0.2]);
+
+figure;
+imagesc(imAlteradaCor)
+title('com cores')
--- Matlab Code/colormap_preprocessing.m
+++ Matlab Code/colormap_preprocessing.m
@@ -7,159 +7,226 @@ nomeSaudaveis=[
     'T0174.1.3.S.2013-03-20.00',
     'T0174.1.4.S.2013-03-20.00',
     'T0174.1.5.S.2013-03-20.00',
+    
     'T0177.1.1.S.2013-03-20.00',
     'T0177.1.2.S.2013-03-20.00',
     'T0177.1.3.S.2013-03-20.00',
     'T0177.1.4.S.2013-03-20.00',
     'T0177.1.5.S.2013-03-20.00',
+    
     'T0182.1.1.S.2013-05-24.00',
     'T0182.1.2.S.2013-05-24.00',
     'T0182.1.3.S.2013-05-24.00',
     'T0182.1.4.S.2013-05-24.00',
     'T0182.1.5.S.2013-05-24.00',
+    
     'T0188.1.1.S.2013-08-12.00',
     'T0188.1.2.S.2013-08-12.00',
     'T0188.1.3.S.2013-08-12.00',
     'T0188.1.4.S.2013-08-12.00',
     'T0188.1.5.S.2013-08-12.00',
+    
     'T0189.1.1.S.2013-09-30.00',
     'T0189.1.2.S.2013-09-30.00',
     'T0189.1.3.S.2013-09-30.00',
     'T0189.1.4.S.2013-09-30.00',
     'T0189.1.5.S.2013-09-30.00',
+    
     'T0190.1.1.S.2013-09-30.00',
     'T0190.1.2.S.2013-09-30.00',
     'T0190.1.3.S.2013-09-30.00',
     'T0190.1.4.S.2013-09-30.00',
     'T0190.1.5.S.2013-09-30.00',
+    
     'T0191.1.1.S.2013-09-06.00',
     'T0191.1.2.S.2013-09-06.00',
     'T0191.1.3.S.2013-09-06.00',
     'T0191.1.4.S.2013-09-06.00',
     'T0191.1.5.S.2013-09-06.00',
+    
     'T0193.1.1.S.2013-10-02.00',
     'T0193.1.2.S.2013-10-02.00',
     'T0193.1.3.S.2013-10-02.00',
     'T0193.1.4.S.2013-10-02.00',
     'T0193.1.5.S.2013-10-02.00',
+    
     'T0194.1.1.S.2013-10-02.00',
     'T0194.1.2.S.2013-10-02.00',
     'T0194.1.3.S.2013-10-02.00',
     'T0194.1.4.S.2013-10-02.00',
     'T0194.1.5.S.2013-10-02.00',
+    
     'T0195.1.1.S.2013-10-07.00',
     'T0195.1.2.S.2013-10-07.00',
     'T0195.1.3.S.2013-10-07.00',
     'T0195.1.4.S.2013-10-07.00',
     'T0195.1.5.S.2013-10-07.00',
+    
     'T0196.1.1.S.2013-10-07.00',
     'T0196.1.2.S.2013-10-07.00',
     'T0196.1.3.S.2013-10-07.00',
     'T0196.1.4.S.2013-10-07.00',
     'T0196.1.5.S.2013-10-07.00',
+    
     'T0199.1.1.S.2013-10-07.00',
     'T0199.1.2.S.2013-10-07.00',
     'T0199.1.3.S.2013-10-07.00',
     'T0199.1.4.S.2013-10-07.00',
     'T0199.1.5.S.2013-10-07.00',
+    
     'T0200.1.1.S.2013-10-07.00',
     'T0200.1.2.S.2013-10-07.00',
     'T0200.1.3.S.2013-10-07.00',
     'T0200.1.4.S.2013-10-07.00',
     'T0200.1.5.S.2013-10-07.00',
+    
     'T0201.1.1.S.2013-10-28.00',
     'T0201.1.2.S.2013-10-28.00',
     'T0201.1.3.S.2013-10-28.00',
     'T0201.1.4.S.2013-10-28.00',
     'T0201.1.5.S.2013-10-28.00',
+    
+    'T0208.1.1.S.2013-10-28.00',
+    'T0208.1.2.S.2013-10-28.00',
+    'T0208.1.3.S.2013-10-28.00',
+    'T0208.1.4.S.2013-10-28.00',
+    'T0208.1.5.S.2013-10-28.00',
+    
+    'T0211.1.1.S.2013-11-08.00',
+    'T0211.1.2.S.2013-11-08.00',
+    'T0211.1.3.S.2013-11-08.00',
+    'T0211.1.4.S.2013-11-08.00',
+    'T0211.1.5.S.2013-11-08.00',
+    
+    'T0212.1.1.S.2013-11-08.00',
+    'T0212.1.2.S.2013-11-08.00',
+    'T0212.1.3.S.2013-11-08.00',
+    'T0212.1.4.S.2013-11-08.00',
+    'T0212.1.5.S.2013-11-08.00',
+    
     'T0216.1.1.S.2013-11-11.00',
     'T0216.1.2.S.2013-11-11.00',
     'T0216.1.3.S.2013-11-11.00',
     'T0216.1.4.S.2013-11-11.00',
     'T0216.1.5.S.2013-11-11.00',
+    
     'T0217.1.1.S.2013-11-11.00',
     'T0217.1.2.S.2013-11-11.00',
     'T0217.1.3.S.2013-11-11.00',
     'T0217.1.4.S.2013-11-11.00',
     'T0217.1.5.S.2013-11-11.00',
+    
     'T0218.1.1.S.2013-11-11.00',
     'T0218.1.2.S.2013-11-11.00',
     'T0218.1.3.S.2013-11-11.00',
     'T0218.1.4.S.2013-11-11.00',
     'T0218.1.5.S.2013-11-11.00',
+    
+    'T0219.1.1.S.2013-11-11.00',
+    'T0219.1.2.S.2013-11-11.00',
+    'T0219.1.3.S.2013-11-11.00',
+    'T0219.1.4.S.2013-11-11.00',
+    'T0219.1.5.S.2013-11-11.00',
+    
     'T0220.1.2.S.2013-11-18.00',
     'T0220.1.3.S.2013-11-18.00',
     'T0220.1.4.S.2013-11-18.00',
     'T0220.1.5.S.2013-11-18.00',
+    
+    'T0221.1.1.S.2013-11-18.00',
+    'T0221.1.2.S.2013-11-18.00',
+    'T0221.1.3.S.2013-11-18.00',
+    'T0221.1.4.S.2013-11-18.00',
+    
     'T0222.1.1.S.2013-11-18.00',
     'T0222.1.2.S.2013-11-18.00',
     'T0222.1.3.S.2013-11-18.00',
     'T0222.1.4.S.2013-11-18.00',
     'T0222.1.5.S.2013-11-18.00',
+    
     'T0224.1.1.S.2013-11-18.00',
     'T0224.1.2.S.2013-11-18.00',
     'T0224.1.3.S.2013-11-18.00',
     'T0224.1.4.S.2013-11-18.00',
     'T0224.1.5.S.2013-11-18.00',
+    
     'T0225.1.1.S.2013-11-18.00',
     'T0225.1.2.S.2013-11-18.00',
     'T0225.1.3.S.2013-11-18.00',
     'T0225.1.4.S.2013-11-18.00',
     'T0225.1.5.S.2013-11-18.00',
+    
     'T0226.1.1.S.2013-11-18.00',
     'T0226.1.2.S.2013-11-18.00',
     'T0226.1.3.S.2013-11-18.00',
     'T0226.1.4.S.2013-11-18.00',
     'T0226.1.5.S.2013-11-18.00',
+    
     'T0233.1.1.S.2013-12-11.00',
     'T0233.1.2.S.2013-12-11.00',
     'T0233.1.3.S.2013-12-11.00',
     'T0233.1.4.S.2013-12-11.00',
     'T0233.1.5.S.2013-12-11.00',
+    
     'T0234.1.1.S.2013-12-11.00',
     'T0234.1.2.S.2013-12-11.00',
     'T0234.1.3.S.2013-12-11.00', 
     'T0234.1.4.S.2013-12-11.00',
     'T0234.1.5.S.2013-12-11.00',
+    
     'T0236.1.1.S.2014-05-21.00',
     'T0236.1.2.S.2014-05-21.00',
     'T0236.1.3.S.2014-05-21.00',
     'T0236.1.4.S.2014-05-21.00',
     'T0236.1.5.S.2014-05-21.00',
+    
     'T0237.1.1.S.2014-05-21.00',
     'T0237.1.2.S.2014-05-21.00',
     'T0237.1.3.S.2014-05-21.00',
     'T0237.1.4.S.2014-05-21.00',
     'T0237.1.5.S.2014-05-21.00',
+    
     'T0238.1.1.S.2014-05-21.00',
     'T0238.1.2.S.2014-05-21.00',
     'T0238.1.3.S.2014-05-21.00',
     'T0238.1.4.S.2014-05-21.00',
     'T0238.1.5.S.2014-05-21.00',
-    'T0239.1.2.S.2014-05-26.00',
-    'T0239.1.3.S.2014-05-26.00',
-    'T0239.1.4.S.2014-05-26.00',
-    'T0239.1.5.S.2014-05-26.00',
+    
     'T0243.1.1.S.2014-04-15.00', 
     'T0243.1.2.S.2014-04-15.00',
     'T0243.1.3.S.2014-04-15.00',
     'T0243.1.4.S.2014-04-15.00',
     'T0243.1.5.S.2014-04-15.00',
+    
     'T0244.1.1.S.2014-05-06.00',
     'T0244.1.2.S.2014-05-06.00',
     'T0244.1.3.S.2014-05-06.00',
     'T0244.1.4.S.2014-05-06.00',
     'T0244.1.5.S.2014-05-06.00',
+    
     'T0259.1.1.S.2014-11-07.00', 
     'T0259.1.2.S.2014-11-07.00',
     'T0259.1.3.S.2014-11-07.00',
     'T0259.1.4.S.2014-11-07.00',
     'T0259.1.5.S.2014-11-07.00',
+    
+    'T0261.1.1.S.2014-11-11.00',
+    'T0261.1.2.S.2014-11-11.00',
+    'T0261.1.3.S.2014-11-11.00',
+    'T0261.1.4.S.2014-11-11.00',
+    'T0261.1.5.S.2014-11-11.00',
+    
     'T0275.1.1.S.2015-03-13.00',
     'T0275.1.2.S.2015-03-13.00',
     'T0275.1.3.S.2015-03-13.00',
     'T0275.1.4.S.2015-03-13.00',
-    'T0275.1.5.S.2015-03-13.00'
+    'T0275.1.5.S.2015-03-13.00',
+    
+    'T0276.1.1.S.2015-03-13.00',
+    'T0276.1.2.S.2015-03-13.00',
+    'T0276.1.3.S.2015-03-13.00',
+    'T0276.1.4.S.2015-03-13.00',
+    'T0276.1.5.S.2015-03-13.00',
 ];
 
 nomeDoentes=[
@@ -168,21 +235,31 @@ nomeDoentes=[
     'T0138.2.3.S.2013-09-06.00',
     'T0138.2.4.S.2013-09-06.00',
     'T0138.2.5.S.2013-09-06.00', 
+    
     'T0179.1.1.S.2013-08-16.00',
     'T0179.1.2.S.2013-08-16.00',
     'T0179.1.3.S.2013-08-16.00',
     'T0179.1.4.S.2013-08-16.00',
     'T0179.1.5.S.2013-08-16.00',
+    
     'T0180.1.1.S.2013-08-16.00',
     'T0180.1.2.S.2013-08-16.00',
     'T0180.1.3.S.2013-08-16.00',
     'T0180.1.4.S.2013-08-16.00',
     'T0180.1.5.S.2013-08-16.00',
+    
     'T0181.1.1.S.2013-08-16.00',
     'T0181.1.2.S.2013-08-16.00',
     'T0181.1.3.S.2013-08-16.00',
     'T0181.1.4.S.2013-08-16.00',
-    'T0181.1.5.S.2013-08-16.00', 
+    'T0181.1.5.S.2013-08-16.00',
+    
+    'T0192.1.1.S.2013-09-06.00',
+    'T0192.1.2.S.2013-09-06.00',
+    'T0192.1.3.S.2013-09-06.00',
+    'T0192.1.4.S.2013-09-06.00',
+    'T0192.1.5.S.2013-09-06.00',
+    
     'T0198.2.1.S.2014-11-11.00',
     'T0198.2.2.S.2014-11-11.00',
     'T0198.2.3.S.2014-11-11.00',
@@ -193,55 +270,92 @@ nomeDoentes=[
     'T0202.1.3.S.2013-10-11.00',
     'T0202.1.4.S.2013-10-11.00',
     'T0202.1.5.S.2013-10-11.00',
+    'T0203.1.1.S.2013-10-11.00',
     'T0203.1.2.S.2013-10-11.00',
     'T0203.1.3.S.2013-10-11.00',
     'T0203.1.4.S.2013-10-11.00',
     'T0203.1.5.S.2013-10-11.00',
+    
     'T0204.1.1.S.2013-10-11.00', 
     'T0204.1.2.S.2013-10-11.00',
     'T0204.1.3.S.2013-10-11.00',
     'T0204.1.4.S.2013-10-11.00',
     'T0204.1.5.S.2013-10-11.00',
+    
     'T0209.1.1.S.2013-11-08.00',
     'T0209.1.2.S.2013-11-08.00',
     'T0209.1.3.S.2013-11-08.00',
     'T0209.1.4.S.2013-11-08.00',
     'T0209.1.5.S.2013-11-08.00',
+    
     'T0210.1.1.S.2013-11-08.00',
     'T0210.1.2.S.2013-11-08.00',
     'T0210.1.3.S.2013-11-08.00',
     'T0210.1.4.S.2013-11-08.00',
     'T0210.1.5.S.2013-11-08.00',
+    
     'T0213.1.1.S.2013-11-08.00', 
     'T0213.1.2.S.2013-11-08.00',
     'T0213.1.3.S.2013-11-08.00',
     'T0213.1.4.S.2013-11-08.00',
     'T0213.1.5.S.2013-11-08.00',
+    
+    'T0240.1.1.S.2014-07-18.00',
+    'T0240.1.2.S.2014-07-18.00',
+    'T0240.1.3.S.2014-07-18.00',
+    'T0240.1.4.S.2014-07-18.00',
+    'T0240.1.5.S.2014-07-18.00',
+    
     'T0241.1.1.S.2014-07-18.00',
     'T0241.1.2.S.2014-07-18.00',
     'T0241.1.3.S.2014-07-18.00',
     'T0241.1.4.S.2014-07-18.00',
     'T0241.1.5.S.2014-07-18.00',
+    
     'T0245.1.1.S.2014-08-22.00',
     'T0245.1.2.S.2014-08-22.00',
     'T0245.1.3.S.2014-08-22.00',
     'T0245.1.4.S.2014-08-22.00',
     'T0245.1.5.S.2014-08-22.00',
+    
+    'T0246.1.1.S.2014-08-22.00',
+    'T0246.1.2.S.2014-08-22.00',
+    'T0246.1.3.S.2014-08-22.00',
+    'T0246.1.4.S.2014-08-22.00',
+    'T0246.1.5.S.2014-08-22.00',
+    
     'T0255.1.1.S.2014-08-22.00', 
     'T0255.1.2.S.2014-08-22.00',
     'T0255.1.3.S.2014-08-22.00',
     'T0255.1.4.S.2014-08-22.00',
     'T0255.1.5.S.2014-08-22.00',
+    
+    'T0256.1.1.S.2014-10-10.00',
+    'T0256.1.2.S.2014-10-10.00',
+    'T0256.1.3.S.2014-10-10.00',
+    'T0256.1.4.S.2014-10-10.00',
+    'T0256.1.5.S.2014-10-10.00',
+    
     'T0257.1.1.S.2014-10-17.00',
     'T0257.1.2.S.2014-10-17.00',
     'T0257.1.3.S.2014-10-17.00',
     'T0257.1.4.S.2014-10-17.00',
     'T0257.1.5.S.2014-10-17.00',
+    
+    'T0258.1.1.S.2014-10-17.00',
+    'T0258.1.3.S.2014-10-17.00',
+    'T0258.1.4.S.2014-10-17.00',
+    'T0258.1.5.S.2014-10-17.00',
     'T0260.1.1.S.2014-11-11.00',
     'T0260.1.2.S.2014-11-11.00',
     'T0260.1.3.S.2014-11-11.00',
     'T0260.1.4.S.2014-11-11.00',
     'T0260.1.5.S.2014-11-11.00',
+    'T0263.1.1.S.2014-12-12.00',
+    'T0263.1.2.S.2014-12-12.00',
+    'T0263.1.3.S.2014-12-12.00',
+    'T0263.1.4.S.2014-12-12.00',
+    'T0263.1.5.S.2014-12-12.00',
     'T0264.1.1.S.2015-01-16.00', 
     'T0264.1.2.S.2015-01-16.00',
     'T0264.1.3.S.2015-01-16.00',
@@ -325,7 +439,7 @@ nomeDoentes=[
 
 %% Leitura imagens saudaveis
 
-saudaveis = '../../Imagens_TXT_Estaticas_Balanceadas/0Saudavel/';
+saudaveis = '../../Imagens_TXT_Estaticas_Balanceadas_allData/0Saudavel/';
 sizeSaudaveis = size(nomeSaudaveis,1);
 pSaudaveis = cell(sizeSaudaveis,1);
 pSaudaveisRGB = cell(sizeSaudaveis,1);
@@ -364,19 +478,64 @@ for i = 1:sizeSaudaveis
     subplot(1,2,2)
     histogram(RGB);
   
-%     folderSaudaveis = strcat('saudaveis/', nomeSaudaveis(i, :), '.png');
-%     saveas(gcf, folderSaudaveis)
+    folderSaudaveis = strcat('saudaveis/', nomeSaudaveis(i, :), '.png');
+    saveas(gcf, folderSaudaveis)
     
+    %Saving original image
     numpyRGB = py.numpy.array(RGB);
-%     folderSaudaveis = strcat('../../Imagens_numpy_array/0Saudaveis/', nomeSaudaveis(i, :));
-%     py.numpy.save(folderSaudaveis, numpyRGB);
+    folderSaudaveis = strcat('../../Imagens_numpy_array_allData/0Saudaveis/', nomeSaudaveis(i, :));
+    py.numpy.save(folderSaudaveis, numpyRGB);
     
+    %getting mean
+    I = img;
+    RGBParsed = RGB;
+    thresh = multithresh(I);
+    seg_I = imquantize(I,thresh);
+    %figure; imagesc(seg_I);
+    
+    RGBParsed1 = RGBParsed(:,:,1);
+    RGBParsed2 = RGBParsed(:,:,2);
+    RGBParsed3 = RGBParsed(:,:,3);
+    meanValue1 = mean(RGBParsed1(seg_I == 1));
+    meanValue2 = mean(RGBParsed2(seg_I == 1));
+    meanValue3 = mean(RGBParsed3(seg_I == 1));
+    
+    %Generating and saving one altered image
+    imOriginal = RGB;
+    tform = randomAffine2d('Rotation',[-45 45], 'XReflection',true,'YReflection',true); 
+    outputView = affineOutputView(size(imOriginal),tform);
+    imAlterada = imwarp(imOriginal,tform,'OutputView',outputView,'FillValues',[meanValue1 meanValue2 meanValue3]);
+    imAlteradaCor = jitterColorHSV(imAlterada,'Contrast',[1.2 1.4],'Saturation',[-0.4 -0.1],'Brightness',[-0.2 0.2]);
+%     figure;
+%     imagesc(imAlteradaCor);
+    folderSaudaveis = strcat('saudaveis/', nomeSaudaveis(i, :), '_alt1.png');
+    saveas(gcf, folderSaudaveis)
+    
+    numpyRGB = py.numpy.array(imAlteradaCor);
+    folderSaudaveis = strcat('../../Imagens_numpy_array_allData/0Saudaveis/', nomeSaudaveis(i, :), '_alt_1');
+    py.numpy.save(folderSaudaveis, numpyRGB);
+    
+    
+    %Generating and saving other altered image
+    tform = randomAffine2d('Rotation',[-45 45], 'XReflection',true,'YReflection',true); 
+    outputView = affineOutputView(size(imOriginal),tform);
+    imAlterada = imwarp(imOriginal,tform,'OutputView',outputView,'FillValues',[meanValue1 meanValue2 meanValue3]);
+    imAlteradaCor = jitterColorHSV(imAlterada,'Contrast',[1.2 1.4],'Saturation',[-0.4 -0.1],'Brightness',[-0.2 0.2]);
+%     figure;
+%     imagesc(imAlteradaCor);
+    folderSaudaveis = strcat('saudaveis/', nomeSaudaveis(i, :), '_alt2.png');
+    saveas(gcf, folderSaudaveis)
+    
+    numpyRGB = py.numpy.array(imAlteradaCor);
+    folderSaudaveis = strcat('../../Imagens_numpy_array_allData/0Saudaveis/', nomeSaudaveis(i, :), '_alt_2');
+    py.numpy.save(folderSaudaveis, numpyRGB);
+   
     close all
 end
 
 %% Leitura imagens doentes
 
-doentes = '../../Imagens_TXT_Estaticas_Balanceadas/1Doente/';
+doentes = '../../Imagens_TXT_Estaticas_Balanceadas_allData/1Doente/';
 sizeDoentes = size(nomeDoentes,1);
 pDoentes = cell(sizeDoentes,1);
 pDoentesRGB = cell(sizeDoentes,1);
@@ -421,7 +580,48 @@ for i = 1:sizeDoentes
     saveas(gcf, folderDoentes)
     
     numpyRGB = py.numpy.array(RGB);
-    folderDoentes = strcat('../../Imagens_numpy_array/1Doentes/', nomeDoentes(i, :));
+    folderDoentes = strcat('../../Imagens_numpy_array_allData/1Doentes/', nomeDoentes(i, :));
+    py.numpy.save(folderDoentes, numpyRGB);
+    
+    %getting mean
+    I = img;
+    RGBParsed = RGB;
+    thresh = multithresh(I);
+    seg_I = imquantize(I,thresh);
+    %figure; imagesc(seg_I);
+    
+    RGBParsed1 = RGBParsed(:,:,1);
+    RGBParsed2 = RGBParsed(:,:,2);
+    RGBParsed3 = RGBParsed(:,:,3);
+    meanValue1 = mean(RGBParsed1(seg_I == 1));
+    meanValue2 = mean(RGBParsed2(seg_I == 1));
+    meanValue3 = mean(RGBParsed3(seg_I == 1));
+    
+    imOriginal = RGB;
+    tform = randomAffine2d('Rotation',[-45 45], 'XReflection',true,'YReflection',true); 
+    outputView = affineOutputView(size(imOriginal),tform);
+    imAlterada = imwarp(imOriginal,tform,'OutputView',outputView,'FillValues',[meanValue1 meanValue2 meanValue3]);
+    imAlteradaCor = jitterColorHSV(imAlterada,'Contrast',[1.2 1.4],'Saturation',[-0.4 -0.1],'Brightness',[-0.2 0.2]);
+%     figure;
+%     imagesc(imAlteradaCor);
+    folderDoentes = strcat('doentes/', nomeDoentes(i, :), '_alt1.png');
+    saveas(gcf, folderDoentes)
+    numpyRGB = py.numpy.array(imAlteradaCor);
+    folderDoentes = strcat('../../Imagens_numpy_array_allData/1Doentes/', nomeDoentes(i, :), '_alt_1');
+    py.numpy.save(folderDoentes, numpyRGB);
+    
+    %Generating and saving other altered image
+    tform = randomAffine2d('Rotation',[-45 45], 'XReflection',true,'YReflection',true); 
+    outputView = affineOutputView(size(imOriginal),tform);
+    imAlterada = imwarp(imOriginal,tform,'OutputView',outputView,'FillValues',[meanValue1 meanValue2 meanValue3]);
+    imAlteradaCor = jitterColorHSV(imAlterada,'Contrast',[1.2 1.4],'Saturation',[-0.4 -0.1],'Brightness',[-0.2 0.2]);
+%     figure;
+%     imagesc(imAlteradaCor);
+    folderDoentes = strcat('doentes/', nomeDoentes(i, :), '_alt2.png');
+    saveas(gcf, folderDoentes)
+    
+    numpyRGB = py.numpy.array(imAlteradaCor);
+    folderDoentes = strcat('../../Imagens_numpy_array_allData/1Doentes/', nomeDoentes(i, :), '_alt_2');
     py.numpy.save(folderDoentes, numpyRGB);
     
     close all
